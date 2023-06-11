55097bc98487f76a8f2959dd305815c530373daa
windyeasy <windyeasy13gb@163.com>
2023-06-11 19:34:52 +0800

    feat: 添加pinia和router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
 <template>
   <div>App</div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
+import router from "./router";
+
 import "normalize.css";
 import "@/assets/css/index.less";
+import store from "./store";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount("#app");
