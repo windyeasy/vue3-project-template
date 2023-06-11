dd97553315352d9e1c58eda1cdb91ba36c13140e
windyeasy <windyeasy13gb@163.com>
2023-06-11 19:44:37 +0800

    feat: 初始化项目

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 
 import "normalize.css";
-import "@/assets/css/index.less";
+import "@/assets/css/index.scss";
 import store from "./store";
 
 const app = createApp(App);
+
 app.use(router);
 app.use(store);
 app.mount("#app");
