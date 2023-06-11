39355b2fb41d11d30e7973e85a1ea4988ae4f4a6
windyeasy <windyeasy13gb@163.com>
2023-06-11 19:30:06 +0800

    feat: 构建目录结构和重置样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import "normalize.css";
+import "@/assets/css/index.less";
+
+createApp(App).mount("#app");
